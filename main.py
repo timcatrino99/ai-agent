aba5f78e6c9a392de194c033ef5fac9b05e042bb
timcatrino99 <timcatrino99@gmail.com>
2025-08-29 14:56:12 -0400

    Added --verbose flag and calculator function

--- main.py
+++ main.py
@@ -4,21 +4,32 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    args_list = []
+    for arg in sys.argv[1:]:
+        if arg[0:2] != "--":
+            args_list.append(arg)
+    if len(args_list) == 0:
+        print("No prompt given. Usage: 'uv run main.py [arguments]'")
+        sys.exit(1)
+    prompt = " ".join(args_list)
+    
     messages = [
-        types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
     ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
